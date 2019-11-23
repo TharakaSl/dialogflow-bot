af82c92c7994e17a48d76d25e2f7d4ce15d8d89c
Sandun Tharaka <spstharaka@gmail.com>
2019-11-24 01:16:27 +0530

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -53,8 +53,7 @@ router.post('/getMovie', (req, res) => {
   });
 }
 else if(req.body.queryResult.action == "input.getUserProfile"){
-  var profileId = req.payload.data.sender.id;
-
+  var profileId = req.body.originalDetectIntentRequest.payload.data.sender.id;
   var profileUrl = `https://graph.facebook.com/v2.6/`+profileId+`?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAAKQWoK91BcBANCZC6ZCOedAmfk4yyNZAlTgtsjnUx1tSpG9TnjZAcPplR44Ki8Y82VxKagul6F1ZBxsDLyncTgO3iYWTtN1wHSXMBNphwSZCPA71kny9GMSc95iEfYZAv7GcTysDUNcs6O0qA4okX6pqDiFTA8LAi5jJicM0ZBpZCv0ZCGPV9o7pvrIWj5pQPIbkZD`;   
   axios.get(profileUrl)
    .then(response => {
