5262fa08504c8ace6b3057816ad00c3719593dc6
Sandun Tharaka <spstharaka@gmail.com>
2019-11-25 11:45:42 +0530

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -87,6 +87,24 @@ else if(req.body.queryResult.action == "input.getUserProfile"){
 }
 
 
+ let messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+	    event = req.body.entry[0].messaging[i]
+	    sender = event.sender.id
+	    // if (event.message && event.message.text) {
+		  //   text = event.message.text
+		  //   if (text === 'Generic') {
+			//     sendGenericMessage(sender)
+			//     continue
+		  //   }
+		  //   sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    // }
+	    if (event.postback) {
+		    text = JSON.stringify(event.postback)
+		    sendMenuMessage(sender)
+		    continue
+	    }
+    }
 
 
 function sendMenuMessage(sender) {
