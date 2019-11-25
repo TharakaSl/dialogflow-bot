ba26288ebbe6454af42c28bf9aa068c36f53bf93
Sandun Tharaka <spstharaka@gmail.com>
2019-11-25 14:16:14 +0530

    format json

--- routes/webhook.js
+++ routes/webhook.js
@@ -23,46 +23,46 @@ var router = express.Router();
 // });
 
 router.post('/', (req, res) => {
-  if(req.body.queryResult.action == "input.getMovieInfo"){
+  if (req.body.queryResult.action == "input.getMovieInfo") {
     var movieName = req.body.queryResult.parameters['movie_name'];
-  axios.get('http://www.omdbapi.com/?t='+movieName+'&apikey=709c821b')
-  .then(function (response) {
-	console.log(response);
-  //res.json(response.data)
-  if(response.data) {
-    let result = response.data;
-    let output = "Average Rating : " + result.imdbRating + 
-    "\n Plot : " + result.Plot + "url" + result.Poster
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({
-          "fulfillmentText" : output
+    axios.get('http://www.omdbapi.com/?t=' + movieName + '&apikey=709c821b')
+      .then(function (response) {
+        console.log(response);
+        //res.json(response.data)
+        if (response.data) {
+          let result = response.data;
+          let output = "Average Rating : " + result.imdbRating +
+            "\n Plot : " + result.Plot + "url" + result.Poster
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify({
+            "fulfillmentText": output
+          }));
+        } else {
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify({
+            "fulfillmentText": "Couldn't find any deatails. :(  "
+          }));
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({
+          "fulfillmentText": "Error. Can you try it again ? "
         }));
-    } else {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({
-        "fulfillmentText" : "Couldn't find any deatails. :(  "
-        }));
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({
-      "fulfillmentText" : "Error. Can you try it again ? "
-                    }));
-  });
-}
-else if(req.body.queryResult.action == "input.getUserProfile"){
-  var config = require('../Payload/welcome.json');
-  var profileId = req.body.originalDetectIntentRequest.payload.data.sender.id;
-  var profileUrl = `https://graph.facebook.com/v2.6/`+profileId+`?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAAKQWoK91BcBANCZC6ZCOedAmfk4yyNZAlTgtsjnUx1tSpG9TnjZAcPplR44Ki8Y82VxKagul6F1ZBxsDLyncTgO3iYWTtN1wHSXMBNphwSZCPA71kny9GMSc95iEfYZAv7GcTysDUNcs6O0qA4okX6pqDiFTA8LAi5jJicM0ZBpZCv0ZCGPV9o7pvrIWj5pQPIbkZD`;   
-  axios.get(profileUrl)
-   .then(response => {
-     console.log(`Hi ` +response.data.first_name);
-     let output = `Hi `+response.data.first_name +`, Hope your day is going well. I can quickly help you with following items `;
-     res.setHeader('Content-Type', 'application/json');
-     res.send(JSON.stringify({
-          "fulfillmentText" : "Hello",
+      });
+  }
+  else if (req.body.queryResult.action == "input.getUserProfile") {
+    var config = require('../Payload/welcome.json');
+    var profileId = req.body.originalDetectIntentRequest.payload.data.sender.id;
+    var profileUrl = `https://graph.facebook.com/v2.6/` + profileId + `?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAAKQWoK91BcBANCZC6ZCOedAmfk4yyNZAlTgtsjnUx1tSpG9TnjZAcPplR44Ki8Y82VxKagul6F1ZBxsDLyncTgO3iYWTtN1wHSXMBNphwSZCPA71kny9GMSc95iEfYZAv7GcTysDUNcs6O0qA4okX6pqDiFTA8LAi5jJicM0ZBpZCv0ZCGPV9o7pvrIWj5pQPIbkZD`;
+    axios.get(profileUrl)
+      .then(response => {
+        console.log(`Hi ` + response.data.first_name);
+        let output = `Hi ` + response.data.first_name + `, Hope your day is going well. I can quickly help you with following items `;
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({
+          "fulfillmentText": "Hello",
           "fulfillmentMessages": [
             {
               "text": {
@@ -72,38 +72,31 @@ else if(req.body.queryResult.action == "input.getUserProfile"){
               }
             },
             {
-              "payload":  config
-            }         
+              "payload": config
+            }
           ]
         }));
-   })
-   .catch(error => {
-    console.log(error);
+      })
+      .catch(error => {
+        console.log(error);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({
+          "fulfillmentText": "Error. Can you try it again ? "
+        }));
+      });
+  }
+  else if (req.body.queryResult.action == "input.healthInformation") {
+    var messageData = require('../Payload/healthInfo.json');
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({
-      "fulfillmentText" : "Error. Can you try it again ? "
-                    }));
-   });
-}
-else if(req.body.queryResult.action == "input.healthInformation"){
-  var messageData = require('../Payload/healthInfo.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
-    "fulfillmentText" : "Hello",
-    "fulfillmentMessages": [
-      {
-        "text": {
-          "text": [
-            "Health Information"
-          ]
+      "fulfillmentText": "Hello",
+      "fulfillmentMessages": [
+        {
+          "payload": messageData
         }
-      },
-      {
-        "payload":  messageData
-      }         
-    ]
-  }));
-}
+      ]
+    }));
+  }
 });
 
 
