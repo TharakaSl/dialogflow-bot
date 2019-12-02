4da4890bf5b574d9cc278806b9266be344af247a
Sandun Tharaka <spstharaka@gmail.com>
2019-12-02 23:51:12 +0530

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -184,11 +184,6 @@ router.post('/', (req, res) => {
   }
   else if (req.body.queryResult.action == "input.RegisterStep1") {
 
-    res.setHeader('Content-Type', 'application/json');
-          res.send(JSON.stringify({
-            "fulfillmentText": "Hello 123"
-          }));
-
     var profileId = req.body.originalDetectIntentRequest.payload.data.sender.id;
     var profileUrl = `https://graph.facebook.com/v2.6/` + profileId + `?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAAKQWoK91BcBANCZC6ZCOedAmfk4yyNZAlTgtsjnUx1tSpG9TnjZAcPplR44Ki8Y82VxKagul6F1ZBxsDLyncTgO3iYWTtN1wHSXMBNphwSZCPA71kny9GMSc95iEfYZAv7GcTysDUNcs6O0qA4okX6pqDiFTA8LAi5jJicM0ZBpZCv0ZCGPV9o7pvrIWj5pQPIbkZD`;
     axios.get(profileUrl)
