1952cf58d0512496d04152f2cd94b8d9e53236e9
Sandun Tharaka <spstharaka@gmail.com>
2019-12-03 16:15:08 +0530

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -249,6 +249,18 @@ router.post('/', (req, res) => {
       ]
     }));
   }
+  else if (req.body.queryResult.action == "input.checkDigit") {
+    var messageData = require('../Payload/healthCondition.json');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+      "fulfillmentText": "Hello",
+      "fulfillmentMessages": [
+        {
+          "payload": messageData
+        }
+      ]
+    }));
+  }
 });
 
 const renderWelcomeMsg = (m) => {
