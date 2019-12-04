b7080f9c9ef3de8e9973a406a336e49c74cb4cfc
Sandun Tharaka <spstharaka@gmail.com>
2019-12-04 21:55:03 +0530

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -59,6 +59,7 @@ router.post('/', (req, res) => {
   }
   else if (req.body.queryResult.action == "input.getUserProfile") {
     var welcomeJson = require('../Payload/welcome.json');
+    var fullTemplate = require('../Payload/activateFull.json');
     var welComeMsg = renderWelcomeMsg(moment());
     var profileId = req.body.originalDetectIntentRequest.payload.data.sender.id;
     var profileUrl = `https://graph.facebook.com/v2.6/` + profileId + `?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAAKQWoK91BcBANCZC6ZCOedAmfk4yyNZAlTgtsjnUx1tSpG9TnjZAcPplR44Ki8Y82VxKagul6F1ZBxsDLyncTgO3iYWTtN1wHSXMBNphwSZCPA71kny9GMSc95iEfYZAv7GcTysDUNcs6O0qA4okX6pqDiFTA8LAi5jJicM0ZBpZCv0ZCGPV9o7pvrIWj5pQPIbkZD`;
@@ -85,7 +86,6 @@ router.post('/', (req, res) => {
           }));
         }
         else {
-          var fullTemplate = require('../Payload/activateFull.json');
           // var scratchCard = require('../Payload/scratchCard.json');
           // var buyNow = require('../Payload/buyNow.json');
           // var findRetailer = require('../Payload/findRetailer.json');
